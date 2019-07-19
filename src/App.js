d2f07369395e56f191f7eeaaaea86ddf848f16d9
Dau Lam <southphillysouth@gmail.com>
2019-07-19 08:55:03 -0400

    Cleanup

--- src/App.js
+++ src/App.js
@@ -2,92 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      activeTab: "self-selected",
-      selfSelectedTrials: [{
-        id: null,
-        value: null,
-        notes: null,
-        type: null
-      }],
-      fastTrials: [{
-        id: null,
-        value: null,
-        notes: null,
-        type: null
-      }]
-    };
-  }
-
-  setActiveTab(tabName) {
-    this.setState({
-      activeTab: tabName,
-      selfSelectedTrials: this.state.selfSelectedTrials,
-      fastTrials: this.state.fastTrials
-    });
-  }
-
-  pushTrial(trial) {
-    trial.type = this.state.activeTab;
-
-    // Push to the active state's array
-    if (this.state.activeTab == 'self-selected') {
-      let trials = this.state.selfSelectedTrials.slice();
-      trial.id = this.state.selfSelectedTrials.length;
-      trials.push(trial);
-
-      // set state
-      this.setState({
-        activeTab: this.state.activeTab,
-        selfSelectedTrials: trials,
-        fastTrials: this.state.fastTrials
-      });
-    }
-    else {
-      let trials = this.state.fastTrials.slice();
-      trial.id = this.state.fastTrials.length;
-      trials.push(trial);
-
-      // set state
-      this.setState({
-        activeTab: this.state.activeTab,
-        selfSelectedTrials: this.state.selfSelectedTrials,
-        fastTrials: trials
-      });
-    }
-  }
-
-  resetActiveTabTrials() {
-    // Look at the current active tab and empty/reset the array
-    if (this.state.activeTab == 'self-selected') {
-      let trials = this.state.selfSelectedTrials.slice();
-      // start at 1 (trial starts 1)
-      trials.length = 1;
-
-      // Set state
-      this.setState({
-        activeTab: this.state.activeTab,
-        selfSelectedTrials: trials,
-        fastTrials: this.state.fastTrials
-      });
-    }
-    else {
-      let trials = this.state.fastTrials.slice();
-      // start at 1 (trial starts 1)
-      trials.length = 1;
-
-      // Set state
-      this.setState({
-        activeTab: this.state.activeTab,
-        selfSelectedTrials: this.state.selfSelectedTrials,
-        fastTrials: trials
-      });
-    }
-  }
-
   render() {
     return (
       <div className="App container">
@@ -124,13 +38,13 @@ class Registration extends Component {
            <div class="form-group row">
              <label for="inputFirstName" class="col-sm-2 col-form-label">First Name</label>
              <div class="col-sm-10">
-               <input type="text" class="form-control" id="inputFirstName"/>
+               <input type="text" class="form-control" id="inputFirstName" />
              </div>
            </div>
            <div class="form-group row">
              <label for="inputLastName" class="col-sm-2 col-form-label">Last Name</label>
              <div class="col-sm-10">
-               <input type="text" class="form-control" id="inputLastName"/>
+               <input type="text" class="form-control" id="inputLastName" />
              </div>
            </div>
            <div class="form-group row">
